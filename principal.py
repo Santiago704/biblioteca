187260da06fc04b0f407cf245341b4524bffc138
santiago <smunera704@gmail.com>
2025-03-19 21:53:03 -0500

    US2/menu principal adicion segundo menu

--- principal.py
+++ principal.py
@@ -1,6 +1,7 @@
 # Importacion de ficheros
-#from login import beginLogin
+from login import login
 from register import register
+from newBook import newBook
 
 # Definicion del menu principal
 def firstMenu():
@@ -28,17 +29,45 @@ def firstMenu():
 # Funcion para ejecutar la opcion seleccionada
 def executeOption(option):
     if option ==1:
-        print('hola')
-        # try:
-        #     users = DAO.listUsers()
-        #     if len(users)>0:
-        #         #Falta ejecutar
-        #     else:
-        #         print('No se ha encontrado usuarios')
-        # except:
-        #     print('Ocurrio un error')
+        user_login = login()
+        user_login.beginLogin()
+        secondMenu()
     elif option ==2:
         user_registration = register()
         user_registration.new_user()
+        secondMenu()
+
+# Funcion para mostrar el segundo menu
+def secondMenu():
+    continueIn = True
+    while(continueIn):
+        correctOption = False
+        while(not correctOption):
+            print('======= MENU SECUNDARIO ======')
+            print('1. Ingresar nuevo libro')
+            print('2. Hacer prestamo de un libro')
+            print('3. Volver al menu inicial')
+            print('=============================')
+            option = int(input('Seleccioneuna opcion'))
+
+            if option <1 or option>3:
+                print('Opcion incorrecta, ingrese nuevamente')
+            elif option ==3:
+                continueIn = False
+                print('Voliste al menu inicial')
+                break
+            else:
+                correctOption = True
+                executeOptionSecondMenu(option)
+
+# Funcion para ejecutar la opcion seleccionada del segundo menu
+def executeOptionSecondMenu(option):
+    if option ==1:
+        new_book = newBook()
+        new_book.new_book_added()
+    elif option ==2:
+        user_registration = register()
+        #user_registration.new_user()
+
 
 firstMenu()
